593106923f950c82e5f6ed14f11b0566f99e9e4e
ashish <ashish2442@gmail.com>
2015-08-13 19:34:26 -0600

    build

---
+++ app/src/main/java/com/example/sms/test/ashish/samplekeyboard/SimpleIME.java
@@ -0,0 +1,106 @@
+package com.example.sms.test.ashish.samplekeyboard;
+
+import android.content.Context;
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
+import android.media.AudioManager;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Created by ashish on 13/08/15.
+ */
+public class SimpleIME extends InputMethodService
+        implements OnKeyboardActionListener{
+
+    private KeyboardView kv;
+    private Keyboard keyboard;
+
+    private boolean caps = false;
+
+    private void playClick(int keyCode){
+        AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
+        switch(keyCode){
+            case 32:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+            case 10:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default: am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
+        }
+    }
+
+    @Override
+    public View onCreateInputView() {
+        kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
+        keyboard = new Keyboard(this, R.xml.qwerty);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return kv;
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection ic = getCurrentInputConnection();
+        playClick(primaryCode);
+        switch(primaryCode){
+            case Keyboard.KEYCODE_DELETE :
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                caps = !caps;
+                keyboard.setShifted(caps);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
+                break;
+            default:
+                char code = (char)primaryCode;
+                if(Character.isLetter(code) && caps){
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code),1);
+        }
+    }
+
+    @Override
+    public void onPress(int primaryCode) {
+    }
+
+    @Override
+    public void onRelease(int primaryCode) {
+    }
+
+    @Override
+    public void onText(CharSequence text) {
+    }
+
+    @Override
+    public void swipeDown() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    }
+
+    @Override
+    public void swipeLeft() {
+    }
+
+    @Override
+    public void swipeRight() {
+    }
+
+    @Override
+    public void swipeUp() {
+    }
+
+}
